c024d886997c3e0cc85679357156fe1b0bb4f26b
durga5237 <katakatladurgaprasad@gmail.com>
2024-02-24 22:29:58 +0530

    EcommerceApplicationUsingSelenium

--- src/test/java/testfiles/testcomponents/Listeners.java
+++ src/test/java/testfiles/testcomponents/Listeners.java
@@ -8,44 +8,42 @@ import com.aventstack.extentreports.ExtentReports;
 
 import basefiles.resources.ExtentReportsNG;
 
-public class Listeners implements ITestListener{
+public class Listeners implements ITestListener {
 	ExtentReports test;
-	ExtentReports re=ExtentReportsNG.getReportObject();
+	ExtentReports re = ExtentReportsNG.getReportObject();
 
 	public void onTestStart(ITestResult result) {
-		
-		test=extent.CreateTest(result.getMethod().getMethodName());
-		
+
 	}
 
 	public void onTestSuccess(ITestResult result) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onTestFailure(ITestResult result) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onTestSkipped(ITestResult result) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onStart(ITestContext context) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onFinish(ITestContext context) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
